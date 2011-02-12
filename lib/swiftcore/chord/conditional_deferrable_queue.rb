cf63cd63b49d578ca187ab6c1ab053b77674c53c
Kirk Haines <wyhaines@gmail.com>
2011-02-12 09:34:00 -0700

    Add a little documentation.

--- lib/swiftcore/chord/conditional_deferrable_queue.rb
+++ lib/swiftcore/chord/conditional_deferrable_queue.rb
@@ -1,5 +1,16 @@
 module Swiftcore
   class Chord
+    # ConditionalDeferrable objects are placed into this queue. Periodically
+    # the queue will be executed, and each deferrable object will have its
+    # conditions evaluated. If the conditions all succeed, then the callback(s)
+    # on it will execute. Those callbacks are able to queue additional future
+    # work. If any of the conditions fail, then the errback(s) on the object
+    # are called. If the deferrable state is unknown, it will be requeued for
+    # evaluation at a later date.
+    # TODO: Make this queue a two layer queue. Items which are repeatedly
+    # TODO: requeued fall into the second layer, and are evaluated less often.
+    # TODO: This would let slow stuff consume fewer resources during their
+    # TODO: condition checks.
     class ConditionalDeferrableQueue
       def initialize
         @queue = []
